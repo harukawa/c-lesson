08052e287de18790dcd371a985d8442a8c4f8259
harukawa <harukawa4k@gmail.com>
2018-11-20 20:48:55 +0000

    def,add,sub,mul,divの作成

--- sources/forth_modoki/interpreter/eval.c
+++ sources/forth_modoki/interpreter/eval.c
@@ -9,15 +9,59 @@ int streq(char *s1, char *s2) {
 	}
 }
 
-void add_op(){
-	struct Node node ={UNKNOWN,{0}};
+static void two_number_pop(int *out_num, int *out_num2) {
+	struct Node node = {UNKNOWN,{0}};
 	stack_pop(&node);
-	int one = node.u.number;
+	*out_num = node.u.number;
 	stack_pop(&node);
-	int two = node.u.number;
-	node.u.number = one+two;
+	*out_num2 = node.u.number;
+}
+
+static void def_op() {
+	struct Node node = {UNKNOWN,{0}};
+	int val;
+	char* literal_name;
+	stack_pop(&node);
+	val = node.u.number;
+	stack_pop(&node);
+	literal_name = node.u.name;
+	node.ntype = NODE_NUMBER;
+	node.u.number = val;
+	dict_put(literal_name, &node);
+}
+
+static void add_op() {
+	struct Node node ={NODE_NUMBER,{0}};
+	int one,two;
+	two_number_pop(&one,&two);
+	node.u.number = two + one;
 	stack_push(&node);
 }
+
+static void sub_op() {
+	struct Node node ={NODE_NUMBER,{0}};
+	int one,two;
+	two_number_pop(&one,&two);
+	node.u.number = two - one;
+	stack_push(&node);
+}
+
+static void mul_op() {
+	struct Node node ={NODE_NUMBER,{0}};
+	int one,two;
+	two_number_pop(&one,&two);
+	node.u.number = two * one;
+	stack_push(&node);
+}
+
+static void div_op() {
+	struct Node node ={NODE_NUMBER,{0}};
+	int one,two;
+	two_number_pop(&one,&two);
+	node.u.number = (int)two / one;
+	stack_push(&node);
+}
+
 
 void eval() {
 	int ch = EOF;
@@ -40,20 +84,10 @@ void eval() {
                 case CLOSE_CURLY:
                     break;
                 case EXECUTABLE_NAME:
-					if(streq(token.u.name, "def")) {
-						int val;
-						char* literal_name;
-						stack_pop(&node);
-						val = node.u.number;
-						stack_pop(&node);
-						literal_name = node.u.name;
-						node.ntype = NODE_NUMBER;
-						node.u.number = val;
-						dict_put(literal_name, &node);
-					} else if(dict_get(token.u.name, &node)) {
+					if(dict_get(token.u.name, &node)) {
 						if(node.ntype == NODE_C_FUNC) {
 							node.u.cfunc();
-					    } else if(dict_get(token.u.name, &node)) {
+						} else if(dict_get(token.u.name, &node)) {
 							stack_push(&node);
 						}
 					}
@@ -73,13 +107,17 @@ void eval() {
 }
 
 
-void register_one_primitive(char *input_key, void (*cfunc)(void)) {
+static void register_one_primitive(char *input_key, void (*cfunc)(void)) {
 	struct Node node ={NODE_C_FUNC, {.cfunc = cfunc}};
 	dict_put(input_key, &node);
 }
 
-void register_primitives(){
+static void register_primitives(){
+	register_one_primitive("def", def_op);
 	register_one_primitive("add", add_op);
+	register_one_primitive("sub", sub_op);
+	register_one_primitive("mul", mul_op);
+	register_one_primitive("div", div_op);
 }
 
 static void test_eval_num_one() {
@@ -154,12 +192,57 @@ static void test_def() {
 	
 }
 
+static void test_eval_sub() {
+    char *input = "5 2 sub";
+    int expect = 3;
+    cl_getc_set_src(input);
+    eval();
+
+    int actual = 0;
+	struct Node actual_data ={UNKNOWN,{0}};
+	stack_pop(&actual_data);
+
+	actual = actual_data.u.number;
+    assert(expect == actual);
+}
+
+static void test_eval_mul() {
+    char *input = "1 2 mul";
+    int expect = 2;
+    cl_getc_set_src(input);
+    eval();
+
+    int actual = 0;
+	struct Node actual_data ={UNKNOWN,{0}};
+	stack_pop(&actual_data);
+
+	actual = actual_data.u.number;
+    assert(expect == actual);
+}
+
+static void test_eval_div() {
+    char *input = "7 2 div";
+    int expect = 3;
+    cl_getc_set_src(input);
+    eval();
+
+    int actual = 0;
+	struct Node actual_data ={UNKNOWN,{0}};
+	stack_pop(&actual_data);
+
+	actual = actual_data.u.number;
+    assert(expect == actual);
+}
+
 
 int main() {
 	register_primitives();
 	test_eval_num_one();
 	test_eval_num_two();
 	test_eval_add();
+	test_eval_sub();
+	test_eval_mul();
+	test_eval_div();
 	test_def();
 	return 1;
 }
