322c4825072ff17d15150c4193420d01f3884bda
harukawa <harukawa4k@gmail.com>
2018-12-31 17:49:59 +0000

    print_hex_blを作成

--- sources/arm_asm/02_arm_assembly/2_4_print_nomem/print_hex_bl.s
+++ sources/arm_asm/02_arm_assembly/2_4_print_nomem/print_hex_bl.s
@@ -1,15 +1,47 @@
+/*
+  arm-none-eabi-as print_hex_bl.s -o print_hex_bl.o
+  arm-none-eabi-ld print_hex_bl.o -Ttext 0x00010000 -o print_hex_bl.elf
+  arm-none-eabi-objcopy print_hex_bl.elf -O binary print_hex_bl.bin
+  qemu-system-arm -M versatilepb -m 128M -nographic -kernel print_hex_bl.bin -serial mon:stdio
+*/
+/*
+  r0 Address of message label
+  r1 Address of UART 
+  r2 ASCII CODE of one letter
+  r3 shift number
+*/
 .globl _start
 _start:
-
-  mov r0, r15
+  ldr r1,=0x101f1000
+  mov r3,#28
+  
+  mov r0, r15 
   bl print_hex
-
   mov r0, #0x68
   bl print_hex
+  b end
 
 end:
   b end
 
-
 print_hex:
-  // TODO: write here!
+    lsr r2,r0,r3
+	and r2,r2,#0x0f
+	cmp r2,#0x0a
+	blt number
+	add r2,r2,#0x27
+
+number:
+	add r2,r2,#0x30
+	str r2,[r1]
+	sub r3,r3,#4
+	cmp r3,#0
+	blt loop_end
+	b print_hex
+
+loop_end:
+	mov r3,#28
+    mov r2,#0x0a
+	str r2,[r1]
+	mov r15,r14
+	
