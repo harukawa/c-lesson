ea3fe8cb4bf0820b6ddc13afd4442a236dcc9d71
harukawa <harukawa4k@gmail.com>
2018-12-31 12:11:26 +0000

    print_hex作成

--- sources/arm_asm/02_arm_assembly/2_3_print_hex/print_hex.s
+++ sources/arm_asm/02_arm_assembly/2_3_print_hex/print_hex.s
@@ -6,9 +6,24 @@
 */
 .globl _start
 _start:
-    ldr r1,=0xdeadbeaf
+    ldr r0,=0x101f1000
+    ldr r1,=0xdea9beaf
+	mov r3,#28
     b print_hex
 print_hex:
-    // TODO: implement here
+    lsr r2,r1,r3
+	and r2,r2,#0x0f
+	cmp r2,#0x0a
+	blt number
+	add r2,r2,#0x27
+number:
+	add r2,r2,#0x30
+	str r2,[r0]
+	sub r3,r3,#4
+	cmp r3,#0
+	blt end
+	b print_hex
+	
 end:
     b end
+
