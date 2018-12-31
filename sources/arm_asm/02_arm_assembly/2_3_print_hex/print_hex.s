d4d928939392d4de247ac5497a2b07971f63d4ff
harukawa <harukawa4k@gmail.com>
2018-12-31 13:13:21 +0000

    コメントを追加

--- sources/arm_asm/02_arm_assembly/2_3_print_hex/print_hex.s
+++ sources/arm_asm/02_arm_assembly/2_3_print_hex/print_hex.s
@@ -4,18 +4,26 @@
  arm-none-eabi-objcopy print_hex.elf -O binary print_hex.bin
  qemu-system-arm -M versatilepb -m 128M -nographic -kernel print_hex.bin -serial mon:stdio
 */
+/*
+  r0 Address of UART 
+  r1 Address of message label
+  r2 ASCII CODE of one letter
+  r3 shift number
+*/
 .globl _start
 _start:
     ldr r0,=0x101f1000
-    ldr r1,=0xdea9beaf
+    ldr r1,=0xdeadbeaf
 	mov r3,#28
     b print_hex
+
 print_hex:
     lsr r2,r1,r3
 	and r2,r2,#0x0f
 	cmp r2,#0x0a
 	blt number
 	add r2,r2,#0x27
+
 number:
 	add r2,r2,#0x30
 	str r2,[r0]
