64a556af94366a5df1f00e99f3e4c48dc8b671de
harukawa <harukawa4k@gmail.com>
2019-01-01 09:16:22 +0000

    putchar_memに実行コマンドのコメントを追加

--- sources/arm_asm/02_arm_assembly/2_5_print_mem/putchar_mem.s
+++ sources/arm_asm/02_arm_assembly/2_5_print_mem/putchar_mem.s
@@ -1,3 +1,16 @@
+/*
+  arm-none-eabi-as putchar_mem.s -o putchar_mem.o
+  arm-none-eabi-ld putchar_mem.o -Ttext 0x00010000 -o putchar_mem.elf
+  arm-none-eabi-objcopy putchar_mem.elf -O binary putchar_mem.bin
+  qemu-system-arm -M versatilepb -m 128M -nographic -kernel putchar_mem.bin -serial mon:stdio
+*/
+/*
+  r0  Address of message label
+  r1  Address of UART 
+  r3  ASCII CODE of one letter
+  r13 Memory
+  r14 Address of return
+*/
 .globl _start
 _start:
   ldr r13,=0x08000000
@@ -7,13 +20,7 @@ _start:
   bl print
 end:
   b end
-/*
-  r0  Address of message label
-  r1  Address of UART 
-  r3  ASCII CODE of one letter
-  r13 Memory
-  r14 Address of return
-*/
+
 putchar:
   // TODO: fix here!
   ldr r1,=0x101f1000
