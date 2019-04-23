7d9596006644e4490721f0db0708136fb70cec9f
harukawa <harukawa4k@gmail.com>
2019-04-23 20:35:19 +0000

    スタックウォークに成功

--- sources/casm_link/04_c_sources/stack_walk2.c
+++ sources/casm_link/04_c_sources/stack_walk2.c
@@ -11,13 +11,37 @@ void print_msg(char *str) {
 void func3 () {
     // TODO: get main_msg from here as target someway.
     char *target;
-
+	//func3 target
+	print_address(((int)&target));
+	
+	print_address(((int)&target)+8);
+	
+	//func2 r11
+	print_address(*((int*)((((int)&target)+8) )));
+	
+	//func2 msg address
+	print_address(*((int*)((((int)&target)+8) ))-8);
+	
+	//*msg
+	print_address(*(  (int*) (*((int*)(((int)&target)+ 8)) -8) ));
+	printf("func2 %s\n",(*(  (int*) (*((int*)(((int)&target)+ 8)) -8) )));
+	
+	//func1 r11
+	print_address(*((int*)((((int)&target)+8) )) );
+	//func1 msg address
+	print_address(*(  (int*) (*((int*)(((int)&target)+ 8)) ) ) -8);
+	
+	//func1 *msg
+	print_address (*((int*)(*(  (int*) (*((int*)(((int)&target)+ 8))  ) ) -8)));
+	
+	printf("func1 %s\n",(*((int*)(*( (int*) (*((int*)(((int)&target)+ 8))   ) ) -8))));
     printf("We are in func3\n");
 }
 
 void func2() {
     char *msg = "func2 message.";
     printf("We are in func2, %s\n", msg);
+	func3();
 }
 
 void func1() {
